722a9ba47e92927bbcb18ebbf0c1743f369c4846
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-09-11 19:12:35 +0200

    create template for site of porte-folio and create home page and porte-folio page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php require "php/header.php"; ?>
+            <main>
+                <section>
+                    <h2> Mon parcours vers le d&eacute; vers le d&eacute;veloppementweb</h2> 
+                    <article>
+                        <h3>Formation d&eacute;vellopeur web</h3>
+                        <p>
+                            J'ai suivi une formation Bootcamp, chez la Wild Code School, en tant que <b>d&eacute;veloppeur
+                            web</b>.
+                        </p>
+                    </article>
+                    <article>
+                        <h3>Mon parcours vers le m&eacute;tier de d&eacute;vellopeur web</h3>
+                        <p>
+                            Il y a quelques ann&eacute;es de cela, j&apos;ai cod&eacute; en autodidacte un site de vente fonctionnel que
+                            vous d&eacute;couvrirez sur la <a href='projet.html'>page projet</a> ainsi que les projets r&eacute;alisés
+                            au sein de la Wild, bien s&ucirc;r sans avoir suivi de formation.
+                        </p>
+                        <p>
+                            Ayant eu un accident du travail, suivi d&apos;un licenciement pour inaptitude professionnel (titulaire
+                            d&apos;une RQTH), et cette premi&egrave;re exp&eacute;rience m&apos;ont naturellement conduit vers une
+                            reconversion vers le <em>de d&eacute;veloppement web</em>.
+                        </p>
+                        <p>
+                            Je suis donc actuellement &agrave; la recherche d&apos;un poste en alternance <strong>d&eacute;veloppeur web
+                            fullstack</strong>, afin d&apos;affin&eacute; mes connaissances, et
+                            d&apos;en acqu&eacute;rir d&apos;autres que je mettrais &agrave; votre service.
+                        </p>
+                    </article>
+                </section>
+            </main>
+<?php require "php/footer.php"; ?>
+	
---
+++ template/footer.php
@@ -0,0 +1,8 @@
+		<footer class='header-footer'>
+			<nav class='footer'>
+				<p>Desingned by Hakim AZIZI<br>
+					&copy; All rights reserved</p>
+			</nav>
+		</footer>
+    </body>
+</html>
