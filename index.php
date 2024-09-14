780a1cab74bebb086a264772efdcdca428fdc66e
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-09-14 22:02:57 +0200

    adding screen of my projects and separate style of header

--- index.php
+++ index.php
@@ -1,33 +1,78 @@
-<?php require "php/header.php"; ?>
-            <main>
-                <section>
-                    <h2> Mon parcours vers le d&eacute; vers le d&eacute;veloppementweb</h2> 
-                    <article>
-                        <h3>Formation d&eacute;vellopeur web</h3>
-                        <p>
-                            J'ai suivi une formation Bootcamp, chez la Wild Code School, en tant que <b>d&eacute;veloppeur
-                            web</b>.
-                        </p>
-                    </article>
-                    <article>
-                        <h3>Mon parcours vers le m&eacute;tier de d&eacute;vellopeur web</h3>
-                        <p>
-                            Il y a quelques ann&eacute;es de cela, j&apos;ai cod&eacute; en autodidacte un site de vente fonctionnel que
-                            vous d&eacute;couvrirez sur la <a href='projet.html'>page projet</a> ainsi que les projets r&eacute;alisés
-                            au sein de la Wild, bien s&ucirc;r sans avoir suivi de formation.
-                        </p>
-                        <p>
-                            Ayant eu un accident du travail, suivi d&apos;un licenciement pour inaptitude professionnel (titulaire
-                            d&apos;une RQTH), et cette premi&egrave;re exp&eacute;rience m&apos;ont naturellement conduit vers une
-                            reconversion vers le <em>de d&eacute;veloppement web</em>.
-                        </p>
-                        <p>
-                            Je suis donc actuellement &agrave; la recherche d&apos;un poste en alternance <strong>d&eacute;veloppeur web
-                            fullstack</strong>, afin d&apos;affin&eacute; mes connaissances, et
-                            d&apos;en acqu&eacute;rir d&apos;autres que je mettrais &agrave; votre service.
-                        </p>
-                    </article>
-                </section>
-            </main>
-<?php require "php/footer.php"; ?>
+<?php require "template/header.php"; ?>
+            <main class='content'>              
+              <section>
+                  <h2> Mon parcours vers le d&eacute;veloppement web</h2> 
+                  <article>
+                      <h3>Formation d&eacute;vellopeur web</h3>
+                      <p>
+                          J'ai suivi une formation Bootcamp, chez la Wild Code School, en tant que <b>d&eacute;veloppeur
+                          web</b>.
+                      </p>
+                  </article>
+                  <article>
+                      <h3>Mon parcours vers le m&eacute;tier de d&eacute;vellopeur web</h3>
+                      <p>
+                          Il y a quelques ann&eacute;es de cela, j&apos;ai cod&eacute; en autodidacte un site de vente fonctionnel que
+                          vous d&eacute;couvrirez sur la <a href='project.html'>page projet</a> ainsi que les projets r&eacute;alisés
+                          au sein de la Wild, bien s&ucirc;r sans avoir suivi de formation.
+                      </p>
+                      <p>
+                          Ayant eu un accident du travail, suivi d&apos;un licenciement pour inaptitude professionnel (titulaire
+                          d&apos;une RQTH), et cette premi&egrave;re exp&eacute;rience m&apos;ont naturellement conduit vers une
+                          reconversion vers le <em>de d&eacute;veloppement web</em>.
+                      </p>
+                      <p>
+                          Je suis donc actuellement &agrave; la recherche d&apos;un poste en alternance <strong>d&eacute;veloppeur web
+                          fullstack</strong>, afin d&apos;affin&eacute; mes connaissances, et
+                          d&apos;en acqu&eacute;rir d&apos;autres que je mettrais &agrave; votre service.
+                      </p>
+                  </article>
+              </section>
+              <section>
+                <h2>Développeur Web</h2>
+                <article>
+                  <h3>Mon expertise en tant que développeur web</h3>
+                  <p>
+                    En tant que développeur web, j'ai acquis une solide expertise dans la création de sites internet performants et adaptés aux besoins spécifiques des clients. Chaque projet que je réalise en tant que développeur web est conçu pour être à la fois ergonomique et optimisé pour les moteurs de recherche.
+                  </p>
+                  <p>
+                    Mon rôle de développeur web consiste à créer des solutions sur mesure en fonction des objectifs de chaque client. Je vous invite à découvrir quelques-unes de mes réalisations dans mon <a href="/portefolio.html">portefolio</a>, où vous trouverez des projets variés.
+                  </p>
+                  <p>
+                    Que ce soit pour un site vitrine ou une boutique en ligne, chaque <a href="/projects.html">projet</a> que je réalise en tant que développeur web est pensé pour offrir la meilleure expérience utilisateur. Explorez mon portefolio et mes projets pour en savoir plus.
+                  </p>
+                </article>
+              </section>
+              <section>
+                <h2><a href="/portefolio.html">Portefolio</a></h2>
+                <article>
+                  <h3>Découvrez mon portefolio de développeur web</h3>
+                  <p>
+                    Mon portefolio met en lumière mon expertise de développeur web à travers une variété de projets. Vous y découvrirez mes meilleures réalisations, conçues pour répondre aux besoins uniques de chaque client et optimiser leur présence en ligne.
+                  </p>
+                  <p>
+                    Chaque projet contenu dans ce portefolio reflète mon engagement en tant que développeur web à fournir des solutions techniques de qualité. Je vous invite également à explorer la page dédiée à mes <a href="/projects.html">projets</a> pour voir comment j'aborde les différentes demandes.
+                  </p>
+                  <p>
+                    Si vous souhaitez en savoir plus sur mon parcours en tant que développeur web, n'hésitez pas à visiter ma page dédiée à mon rôle de <a href="./">développeur web</a>, où j'explique en détail mes compétences et mon approche du développement web.
+                  </p>
+                </article>
+              </section>
+              <section>
+                <h2><a href="/projects.html">Projets</a></h2>
+                <article>
+                  <h3>Mes projets en tant que développeur web</h3>
+                  <p>
+                    En tant que développeur web, chaque projet est une occasion de relever de nouveaux défis techniques. Je m'assure de toujours proposer des solutions adaptées aux besoins des clients, tout en garantissant des performances optimales.
+                  </p>
+                  <p>
+                    Mon expertise dans le développement web se reflète à travers les différents <a href="/portefolio.html">projets présentés dans mon portefolio</a>. Vous pourrez y explorer les diverses réalisations que j'ai créées au fil des ans.
+                  </p>
+                  <p>
+                    Si vous souhaitez en savoir plus sur mes compétences en tant que <a href="./">développeur web</a> et voir comment je peux vous aider à mener à bien votre projet, n'hésitez pas à consulter les autres sections de mon site.
+                  </p>
+                </article>
+              </section>
+        </main>
+<?php require "template/footer.php"; ?>
 	
--- template/header.php
+++ template/header.php
@@ -13,201 +13,21 @@ if($real_url==='/index.php'){$page_title='Hakim AZIZI';}
 		<meta name='contactstate' content='France'>
 		<meta name='contactcity' content='Nantes'>
 		<meta name='language' content='fr'>
-		<style>
-
-			* {
-				margin: 0;
-				padding: 0;
-				color: #000;
-			}
-
-			body {
-				background: #DCE7FF;
-				background-repeat: no-repeat;
-				background-size: cover;
-				color: #f7efef;
-			}
-
-			.header>nav{
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-				padding:0 1rem;
-				background: #000;
-				height: 100px;
-			}
-
-			.header>nav>ul {
-				display: flex;
-				flex-wrap: wrap;
-				justify-content: flex-end;
-				align-items: center;
-				gap: 1rem;
-			}
-
-			.header>nav>p,
-			.header>nav>ul>li {
-				text-align: center;
-				list-style: none;
-				background-color: #f7efef;
-				height: 50px;
-				border-radius: 50%;
-				width: 100px;
-			}
-
-			.header>nav>p:hover,
-			.header>nav>ul>li:hover {
-				font-weight: bold;
-				cursor: pointer;
-				background: #000;
-				border: 2px solid #f7efef;
-			}
-
-			.header>nav>p>a,
-			.header>nav>ul>li>a {
-				text-decoration: none;
-				color: #000;
-				display: block;
-				width: 100%;
-				height: 100%;
-				line-height: 50px;
-				margin-top: auto;
-				margin-bottom: auto;
-			}
-
-			.header>nav>p>a:hover,
-			.header>nav>ul>li>a:hover {
-				color: #f7efef;
-			}
-
-			.content,
-			.title {
-				margin: 1rem;
-			}
-
-			h1 {
-				text-align: center;
-				font-size: 2rem;
-			}
-
-			h2 {
-				font-size: 1.5rem;
-			}
-
-			h3,
-			p {
-				font-size: 1.2rem;
-			}
-
-			.content {
-				display: flex;
-				justify-content: center;
-				flex-wrap: wrap;
-				gap: 2rem;
-			}
-
-			.content>div,
-			.content>section{
-				width:47%;
-				min-width: 300px;
-			}
-
-			.content2 {
-				overflow: hidden;
-			}
-
-			#resize {
-				display: flex;
-				flex-wrap: nowrap;
-				gap: 2%;
-				width: 304%;
-			}
-
-			.slide {
-				width: 32%;
-				display:block;
-			}
-
-			.slide>img{
-				width:100%;
-				height:auto;
-			}
-
-			form {
-				display: flex;
-				align-items: center;
-				flex-direction: column;
-				gap: 1rem;
-			}
-
-			label {
-				width: 250px;
-			}
-
-			input,
-			textarea,
-			button {
-				padding: .2rem .5rem;
-				width: 250px;
-				border-radius: 51px;
-				border: none;
-				background: #F2F6FF;
-			}
-
-			.adjust {
-				margin-top: -1rem;
-				display: flex;
-				justify-content: space-between;
-			}
-
-			.adjust:hover {
-				cursor: pointer;
-			}
-			
-			.adjust>input {
-				width: 12px;
-				height: 12px;
-			}
-
-			.adjust>input:hover {
-				cursor: pointer;
-			}
-
-			textarea {
-				height: 250px;
-				border-radius: 15px;
-			}
-
-			button:hover {
-				background: #C0C5ED;
-				cursor: pointer;
-			}
-
-			footer {
-				margin-top: 2rem;
-			}
-
-			.footer {
-				display: flex;
-				background: #000;
-				align-items: center;
-				gap: 2rem;
-			}
-
-			.footer>address,
-			.footer>p {
-				color: #f7efef;
-			}
-
-		</style>
+		<link href="style/style.css" rel="stylesheet" type="text/css">
+<?php if($page_title==='contact'){ ?>
+		<link href="style/contact.css" rel="stylesheet" type="text/css">
+<?php } ?>
+<?php if($page_title==='portefolio'){ ?>
+		<link href="style/portefolio.css" rel="stylesheet" type="text/css">
+<?php } ?>
 	</head>
 	<body>
 		<header class='header'>
 			<nav>
-				<p><a href='./'>accueil</a></p>
+				<p><a href='./'>Accueil</a></p>
 				<ul>
-					<li><a href='porte-folio.html'>Porte folio</a></li>
-					<li><a href='project.html'>Projet</a></li>
+					<li><a href='portefolio.html'>Portefolio</a></li>
+					<li><a href='projects.html'>Projets</a></li>
 					<li><a href='collaboration.html'>Collaboration</a></li>
 					<li><a href='contact.html'>Contact</a></li>
 				</ul>
