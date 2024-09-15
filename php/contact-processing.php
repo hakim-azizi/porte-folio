387a2ecf5cca06dbc54e2d9dcf3e62aba693f132
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-09-15 22:28:48 +0200

    update url rewritting, update content home page, update style

--- php/contact-processing.php
+++ php/contact-processing.php
@@ -28,7 +28,11 @@ if(!empty($_POST['boat1']) OR $validate_post!==$validate){$response='La validati
 	if(!preg_match("#^([A-Za-z][a-zA-Z0-9_.\- ]{5,1000})$#",$_POST['message'])){
 		$response='Une erreur c&apos;est produite, veuillez réessayer plus tard.';
 	}else{
-		$message=$_POST['message'];
+		$message='name : '.$name.'<br />';
+		$message.='telephone : '.$tel.'<br />';
+		$message.='email : '.$email.'<br />';
+		$message.='----------------------------------------------- <br />';
+		$message.=$_POST['message'];
 	}
 	if(preg_match("#^([A-Za-z]{2})$#",$_POST['receive-mail'])){
 		$receive_mail=$_POST['receive-mail'];
@@ -47,7 +51,7 @@ if($response===''){
 		$response='L&apos;email a bien &eacute;t&eacute; envoy&eacute;.';
 		if(@$receive_mail){mail($email,$subject,$message,$header1);}
 	}else{
-		$response='Une erreur c&apos;est produite, veuillez réessayer plus tard.';
+		$response='Une erreur c&apos;est produite, veuillez réessayer plus tard envoie.';
 	}
 }
 
--- template/footer.php
+++ template/footer.php
@@ -4,5 +4,19 @@
 					&copy; All rights reserved</p>
 			</nav>
 		</footer>
+		<script>
+				let open=0;
+				function openMenu(){
+					if(open===0){
+						document.querySelector('#show-menu').style.display="flex";
+						document.querySelector('.menu').setAttribute("src", "<?php echo $adjust_url; ?>images/menu-burger1.png");
+						open=1;
+					}else{
+						document.querySelector('#show-menu').style.display="none";
+						document.querySelector('.menu').setAttribute("src", "<?php echo $adjust_url; ?>images/menu-burger.png");
+						open=0;
+					}
+				};
+			</script>
     </body>
 </html>
