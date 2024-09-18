2fca3b0a36c4435dbd0e314b13c985a960ebb243
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-09-18 17:27:42 +0200

    fixed dead links

--- portefolio.php
+++ portefolio.php
@@ -29,7 +29,7 @@ $url3=str_replace('.jpg','.html',$array_picture[$id_photo3]);
 			<section class='content1'>
 				<h2><a href='projects.html'>Projets</a> :</h2>
 				<article>
-					<h3><a href='projet/la-rose-de-l-orient.html'>La Rose de l&apos;Orient</a> :</h3>
+					<h3><a href='projects/la-rose-de-l-orient.html'>La Rose de l&apos;Orient</a> :</h3>
 					<p>Vous d&eacute;couvrirez la page d&eacute;di&eacute;e au site que j&apos;ai con&ccedil;u avec quelque
 						sp&eacute;cification technique ainsi, qu&apos;un screenshot de la page d&apos;accueil, les langages
 						que j&apos;ai utilis&eacute; : HTML, CSS, JavaScript, Apache (Configuration du fichier php.ini,
@@ -37,30 +37,30 @@ $url3=str_replace('.jpg','.html',$array_picture[$id_photo3]);
 						PHP, SQL.</p>
 				</article>
 				<article>
-					<h3><a href='projet/robe-et-vetement.html'>Projet Robe et V&ecirc;tement</a> :</h3>
+					<h3><a href='projects/robe-et-vetement.html'>Projet Robe et V&ecirc;tement</a> :</h3>
 					<p>Un petit projet en HTML, CSS, JavaScript, vous y trouverez &eacute;galement une page d&eacute;dier
 						&agrave; ce petit projet.</p>
 				</article>
 				<article>
-					<h3><a href='projet/the-wild-fork.html'>Projet The Wild Fork</a> :</h3>
+					<h3><a href='projects/the-wild-fork.html'>Projet The Wild Fork</a> :</h3>
 					<p>Ce site est le premier projet de ma formation Bootcamp de 5 mois, sur cette page, vous y trouverez
 						aussi un descriptif technique, mais aussi un screenshot de la page d&apos;accueil. Ce site codé en équipe de 4 a
 						&eacute;t&eacute; r&eacute;alis&eacute; en HTML, CSS, JavaScript.</p>
 				</article>
 				<article>
-					<h3><a href='projet/faketory.html'>Projet Faketory</a> :</h3>
+					<h3><a href='projects/faketory.html'>Projet Faketory</a> :</h3>
 					<p>Ce site est le deuxi&egrave;me projet codé en équipe de ma formation Bootcamp de 5 mois, sur cette page, vous y
 						trouverez aussi un descriptif technique, mais aussi un screenshot de la page d&apos;accueil. Ce site codé en équipe
 						 de 4 a &eacute;t&eacute; r&eacute;alis&eacute; en React.js.</p>
 				</article>
 				<article>
-					<h3><a href='projet/afac974.html'>Projet AFAC974</a> :</h3>
+					<h3><a href='projects/afac974.html'>Projet AFAC974</a> :</h3>
 					<p>Ce site est le dernier projet de ma formation Bootcamp de 5 mois, sur cette page, vous y
 						trouverez aussi un descriptif technique, mais aussi un screenshot de la page d&apos;accueil. Ce site codé en autonomie 
 						a &eacute;t&eacute; r&eacute;alis&eacute; en React.js, Express.js associer à node.js et une base de donnée SQL.</p>
 				</article>
 				<article>
-					<h3><a href='projet/carte-postale.html'>Projet carte postale</a> :</h3>
+					<h3><a href='projects/carte-postale.html'>Projet carte postale</a> :</h3>
 					<p>Lors de ma formation nous devions réalisé en 2 jours un site de carte postale, en deux
 						a &eacute;t&eacute; r&eacute;alis&eacute; en React.js, Express.js associer à node.js.</p>
 				</article>
