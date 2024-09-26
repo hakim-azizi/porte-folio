d5ae8240610cb99bbc1f4ef93f0b12cdb412b7ec
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-09-26 10:38:46 +0200

    udpate any design page and fix bug

--- template/header.php
+++ template/header.php
@@ -12,41 +12,45 @@ require './php/security.php';
 		<meta name='contactcity' content='Nantes'>
 		<meta name='language' content='fr'>
 		<link href="<?php echo $adjust_url; ?>style/style.css" rel="stylesheet" type="text/css">
-<?php if($title==='Hakim AZIZI'){ ?>
-	<link href="style/home.css" rel="stylesheet" type="text/css">
-<?php } ?>
 <?php if($title==='contact'){ ?>
 	<link href="style/contact.css" rel="stylesheet" type="text/css">
 <?php } ?>
 <?php if($title==='portefolio'){ ?>
 	<link href="style/portefolio.css" rel="stylesheet" type="text/css">
 <?php } ?>
-<?php if($title==='projet'){ ?>
+<?php if($title==='projets'){ ?>
 	<link href="style/projects.css" rel="stylesheet" type="text/css">
 <?php } ?>
 	</head>
 	<body>
 		<header class='navbar'>
 			<nav>
-				<p><a href='../'>Accueil</a></p>
+				<p><a href='../' <?php if($title==="Hakim AZIZI"){ ?>class='current'<?php } ?>>Accueil</a></p>
 				<ul id='show-menu'>
-					<li><a href='../web-developer.html'>d&eacute;veloppeur web</a></li>
-					<li><a href='../cv-hakim-azizi.pdf'>CV</a></li>
-					<li><a href='../portefolio.html'>Portefolio</a></li>
-					<li id='show-projects'  onclick="openProjects()">Voir les projets
+					<li><a href='../web-developer.html' <?php if($title==="d&eacute;veloppeur web"){ ?>class='current'<?php } ?>>d&eacute;veloppeur web</a></li>
+					<li><a href='../cv-hakim-azizi.html' <?php if($title==="CV Hakim AZIZI"){ ?>class='current'<?php } ?>>CV</a></li>
+					<li><a href='../portefolio.html' <?php if($title==="portefolio"){ ?>class='current'<?php } ?>>Portefolio</a></li>
+					<li id='show-projects' onclick="openProjects()"><span <?php if(strstr($title,'projet')){ ?>class='current'<?php } ?>>Voir les projets</span>
 					<ul id='projects'>
 					<?php 
 						while($projects=mysqli_fetch_array($data_projects)){
 							?>
-							<li><?php echo $projects['name']; ?></li>
-						<?php
+							<li><a href='../projects/<?php echo $projects['name']; ?>.html'<?php
+							if($title==='projet AFAC974'){ ?>class='current'<?php }
+							elseif($title==='projet cr&eacute;ation de carte postale'){ ?>class='current'<?php }
+							elseif($title==='projet Faketory'){ ?>class='current'<?php }
+							elseif($title==='projet la Rose de l&apos;Orient'){ ?>class='current'<?php }
+							elseif($title==='projet Robe et V&ecirc;tement'){ ?>class='current'<?php }
+							elseif($title==='projet THE Wild FORK'){ ?>class='current'<?php }
+							?>><?php echo $projects['name']; ?></a></li>
+					<?php
 						}
-						?>
+					?>
 					</ul>				
 				</li>
-					<li><a href='../collaboration.html'>Collaboration</a></li>
-					<li><a href='../contact.html'>Contact</a></li>
-					<li><a href='../partner.html'>Partenaire</a></li>
+					<li><a href='../collaboration.html' <?php if($title==="collaboration"){ ?>class='current'<?php } ?>>Collaboration</a></li>
+					<li><a href='../contact.html' <?php if($title==="contact"){ ?>class='current'<?php } ?>>Contact</a></li>
+					<li><a href='../partner.html' <?php if($title==="partenaire"){ ?>class='current'<?php } ?>>Partenaire</a></li>
 				</ul>
 				<img class='menu' src="../images/menu-burger.png" alt="ouvrir le menu" onclick="openMenu()">
 			</nav>
--- web-developer.php
+++ web-developer.php
@@ -1,9 +1,33 @@
 <?php
-require 'template/header.php';
+$picture = scandir('./photo');
+$lenght_array = count($picture);
+$number_picture=0;
+$array_picture=[];
+for($i=0;$i<$lenght_array;$i++){
+	if(strstr($picture[$i],'.jpg')){
+		$number_picture++;
+		array_push($array_picture,$picture[$i]);
+	}
+}
+require "template/header.php"; ?>
+<aside class="headband"><figure>
+<?php
+for($i=0;$i<$number_picture;$i++){
 ?>
+<a href='projects/<?php echo str_replace('.jpg','.html',$array_picture[$i]); ?>'><img src='photo/<?php echo $array_picture[$i]; ?>' alt=''></a>
+<?php
+}
+?>
+</figure></aside>
             <main class='content'>
               <section>
                 <h2>Développeur Web</h2>
+                <div>
+                <aside>
+                        <figure>
+                            <img src='images/df66c823-29fc-40a8-8402-3e4251a59660409.JPG' alt='d&eacute;veloppeur web hakim AZIZI' width='250px' height='auto'>
+                        </figure>
+                    </aside>
                 <article>
                   <h3>Mon expertise en tant que développeur web</h3>
                   <p>
@@ -16,6 +40,7 @@ require 'template/header.php';
                     Que ce soit pour un <strong>site vitrine ou une boutique en ligne</strong>, chaque <em><a href="/projects.html">projet</a></em> que je <strong>réalise en tant que développeur web</strong> est pensé pour offrir la meilleure <strong>expérience utilisateur</strong>. Explorez mon <em>portefolio et mes projets</em> pour en savoir plus.
                   </p>
                 </article>
+                </div>
               </section>              
             </main>
 <?php
