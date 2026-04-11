a18a75bb32f19ca6e2ef090a80a5765226bc257a
Hakim Azizi <160917355+hakim-azizi@users.noreply.github.com>
2026-04-11 22:43:26 +0200

    Refactor database connection logic
    Updated database connection credentials and added error handling.

--- database/database.php
+++ database/database.php
@@ -5,8 +5,8 @@ header("Status:301 Moved Permanently", false, 301);
 header("location:https://web-developer-nantes.com".$_SERVER["REQUEST_URI"]."");
 exit();
 }
-if(!$connectionbd=mysqli_connect("localhost","root","20-Juillet-1976","webdk3_webdeveloper")){echo 'ERREUR !!!';}
+if(!$connectionbd=mysqli_connect("localhost","root","**********","webdk3_webdeveloper")){echo 'ERREUR !!!';}
 }else{
-     $connectionbd=mysqli_connect("webdqx-webdeveloper.db.tb-hosting.com","webdqx_hakazizi","20-Juillet-1976" ,"webdqx_webdeveloper") OR diemysql_error('erreur de connexion');
+     $connectionbd=mysqli_connect("*********","webdqx_hakazizi","***********" ,"***********") OR diemysql_error('erreur de connexion');
 }
 ?>
