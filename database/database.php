9653c67ec4e87dd6653b32aa5c9568eaf2243989
Hakim Azizi <160917355+hakim-azizi@users.noreply.github.com>
2026-04-11 22:44:18 +0200

    Refactor database connection handling in database.php

--- database/database.php
+++ database/database.php
@@ -5,7 +5,7 @@ header("Status:301 Moved Permanently", false, 301);
 header("location:https://web-developer-nantes.com".$_SERVER["REQUEST_URI"]."");
 exit();
 }
-if(!$connectionbd=mysqli_connect("localhost","root","**********","webdk3_webdeveloper")){echo 'ERREUR !!!';}
+if(!$connectionbd=mysqli_connect("localhost","root",'',"webdk3_webdeveloper")){echo 'ERREUR !!!';}
 }else{
      $connectionbd=mysqli_connect("*********","webdqx_hakazizi","***********" ,"***********") OR diemysql_error('erreur de connexion');
 }
